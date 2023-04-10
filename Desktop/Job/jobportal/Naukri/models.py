5eeb30fa9fda6fe621b9b770873dcb289f617162
Your Name <you@example.com>
2023-04-10 09:19:11 +0530

    Second

--- Desktop/Job/jobportal/Naukri/models.py
+++ Desktop/Job/jobportal/Naukri/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class signup(models.Model):
+    name=models.CharField(max_length=50)
+    email=models.EmailField(max_length=50)
+    password=models.CharField(max_length=50)
+
+class login(models.Model):
+    email=models.EmailField(max_length=50)
+    password=models.CharField(max_length=50)    
